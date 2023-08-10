4f06e13c4de8c6de61da452558b5f903ef252ca3
zhjohnchan <gdst_czh@163.com>
2023-08-10 09:49:15 -0700

    init

--- modules/metrics.py
+++ modules/metrics.py
@@ -1,3 +1,5 @@
+from sklearn.metrics import roc_auc_score, f1_score, recall_score, precision_score
+
 from pycocoevalcap.bleu.bleu import Bleu
 from pycocoevalcap.meteor import Meteor
 from pycocoevalcap.rouge import Rouge
@@ -31,3 +33,21 @@ def compute_scores(gts, res):
         else:
             eval_res[method] = score
     return eval_res
+
+
+def compute_mlc(gt, pred, label_set):
+    res_mlc = {}
+    avg_aucroc = 0
+    for i, label in enumerate(label_set):
+        res_mlc['AUCROC_' + label] = roc_auc_score(gt[:, i], pred[:, i])
+        avg_aucroc += res_mlc['AUCROC_' + label]
+    res_mlc['AVG_AUCROC'] = avg_aucroc / len(label_set)
+
+    res_mlc['F1_MACRO'] = f1_score(gt, pred, average="macro")
+    res_mlc['F1_MICRO'] = f1_score(gt, pred, average="micro")
+    res_mlc['RECALL_MACRO'] = recall_score(gt, pred, average="macro")
+    res_mlc['RECALL_MICRO'] = recall_score(gt, pred, average="micro")
+    res_mlc['PRECISION_MACRO'] = precision_score(gt, pred, average="macro")
+    res_mlc['PRECISION_MICRO'] = precision_score(gt, pred, average="micro")
+
+    return res_mlc
